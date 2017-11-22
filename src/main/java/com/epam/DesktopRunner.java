0da6ac7ba4ae8e0e4f44c01af8fe57ddddd57475
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-22 17:08:40 +0300

    drop abstract runner

--- src/main/java/com/epam/DesktopRunner.java
+++ src/main/java/com/epam/DesktopRunner.java
@@ -1,9 +1,10 @@
 package com.epam;
 
+import com.epam.utils.PropertyManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class DesktopRunner extends AbstractRunner {
+public class DesktopRunner {
 
     private static final Logger LOGGER = LogManager.getLogger(DesktopRunner.class);
 
@@ -11,5 +12,9 @@ public class DesktopRunner extends AbstractRunner {
         LOGGER.info("Desktop Runner");
         setSystemProperties();
         LOGGER.info("Browser: " + System.getProperty("browser.name"));
+    }
+
+    static void setSystemProperties() {
+        System.setProperty("browser.name", PropertyManager.get("browser.name"));
     }
 }
--- src/main/java/com/epam/MobileRunner.java
+++ src/main/java/com/epam/MobileRunner.java
@@ -3,7 +3,7 @@ package com.epam;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class MobileRunner extends AbstractRunner {
+public class MobileRunner extends DesktopRunner {
 
     private static final Logger LOGGER = LogManager.getLogger(MobileRunner.class);
 
