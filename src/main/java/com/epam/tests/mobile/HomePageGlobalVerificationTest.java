cfc5d75854fbbb36127ca9d907013349f6310920
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-23 14:42:13 +0300

    add simple test for mobile

---
+++ src/main/java/com/epam/tests/mobile/HomePageGlobalVerificationTest.java
@@ -0,0 +1,21 @@
+package com.epam.tests.mobile;
+
+import com.epam.pages.HeaderLayout;
+import com.epam.pages.bean.MenuItem;
+import com.epam.pages.mobile.MobileHomePage;
+import com.epam.tests.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class HomePageGlobalVerificationTest extends BaseTest {
+
+    @Test
+    public void homePageGlobalVerificationTest() {
+        HeaderLayout homePage = new MobileHomePage(driver).openMenu();
+        List<MenuItem> menuItems = homePage.getPrimaryMenuItems();
+
+        Assert.assertEquals(menuItems.size(), 6);
+    }
+}
