4ff63c35a9d86fb3761abcff06edfc78e8b3b308
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-23 14:37:27 +0300

    add base abstract page

---
+++ src/main/java/com/epam/pages/BasePage.java
@@ -0,0 +1,14 @@
+package com.epam.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class BasePage {
+
+    protected final WebDriver driver;
+
+    protected BasePage (WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+}
