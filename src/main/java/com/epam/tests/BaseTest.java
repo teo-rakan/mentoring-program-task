aa8e69f37e779ee4c39da12ead1a8675dc8727d7
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-24 13:40:39 +0300

    add DriverManager injection using Guice

--- src/main/java/com/epam/tests/BaseTest.java
+++ src/main/java/com/epam/tests/BaseTest.java
@@ -1,10 +1,25 @@
 package com.epam.tests;
 
-import org.openqa.selenium.WebDriver;
+import com.epam.core.guice.GuiceInjector;
+import com.epam.core.webdriver.DriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.BeforeClass;
+
+import javax.inject.Inject;
 
 public class BaseTest {
 
-    //todo inject this
-    protected WebDriver driver;
+    private static final Logger LOGGER = LogManager.getLogger(BaseTest.class);
 
+    @Inject
+    DriverManager driverManager;
+
+    @BeforeClass(description = "Inject members")
+    public void inject() {
+        GuiceInjector.getInjector().injectMembers(this);
+
+        LOGGER.debug("Open home page");
+        driverManager.open("http://www.sho.com/");
+    }
 }
