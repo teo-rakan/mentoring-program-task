d8a85e0fd410dddd6f0ae35df90452aedb711c9b
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-24 13:40:11 +0300

    add DriverManager & FieldDecorator injection using Guice

--- src/main/java/com/epam/pages/BasePage.java
+++ src/main/java/com/epam/pages/BasePage.java
@@ -1,14 +1,31 @@
 package com.epam.pages;
 
-import org.openqa.selenium.WebDriver;
+import com.epam.core.guice.GuiceInjector;
+import com.epam.core.webdriver.DriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.FieldDecorator;
+
+import javax.inject.Inject;
 
 public abstract class BasePage {
 
-    protected final WebDriver driver;
+    private static final Logger LOGGER = LogManager.getLogger(BasePage.class);
 
-    protected BasePage (WebDriver driver) {
-        this.driver = driver;
+    @Inject
+    protected DriverManager driverManager;
+
+    @Inject
+    private FieldDecorator fieldDecorator;
+
+    protected BasePage () {
+        GuiceInjector.getInjector().injectMembers(this);
+
+        LOGGER.debug("Objects injected via Guice:");
+        LOGGER.debug("Driver manager: " + driverManager.getClass().getSimpleName());
+        LOGGER.debug("Field decorator: " + fieldDecorator.getClass().getSimpleName());
+
+        PageFactory.initElements(fieldDecorator, this);
     }
-
-
 }
