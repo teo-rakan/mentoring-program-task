de4fc2e042d055469d700d2c1055e26879e8f0c8
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-24 17:13:40 +0300

    add simple chrome driver settings

--- src/main/java/com/epam/core/webdriver/ChromeDriverManager.java
+++ src/main/java/com/epam/core/webdriver/ChromeDriverManager.java
@@ -1,9 +1,17 @@
 package com.epam.core.webdriver;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+
 public class ChromeDriverManager extends DriverManager {
 
     @Override
     void createDriver() {
-
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 }
