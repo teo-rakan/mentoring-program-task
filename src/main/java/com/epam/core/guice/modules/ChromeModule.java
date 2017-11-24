89b51b4a661f9dffe11878f85ff13bbafbf256fa
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-24 17:12:14 +0300

    rename from ChromeDesktopModule

--- src/main/java/com/epam/core/guice/modules/ChromeModule.java
+++ src/main/java/com/epam/core/guice/modules/ChromeModule.java
@@ -2,21 +2,26 @@ package com.epam.core.guice.modules;
 
 import com.epam.core.webdriver.ChromeDriverManager;
 import com.epam.core.webdriver.DriverManager;
-import com.google.inject.Provides;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
 import org.openqa.selenium.support.pagefactory.DefaultFieldDecorator;
 import org.openqa.selenium.support.pagefactory.FieldDecorator;
 
-public class ChromeDesktopModule extends GuiceModule {
+public class ChromeModule extends GuiceModule {
 
-    @Provides
+    private static final Logger LOGGER = LogManager.getLogger(ChromeModule.class);
+
+    @Override
     public DriverManager provideDriverManager() {
+        LOGGER.debug("ChromeDriverManager was provided");
         return new ChromeDriverManager();
     }
 
     @Override
     public FieldDecorator provideFieldDecorator(DriverManager driverManager) {
+        LOGGER.debug("DefaultFieldDecorator was provided");
         WebDriver driver = driverManager.getDriver();
         return new DefaultFieldDecorator(new DefaultElementLocatorFactory(driver));
     }
