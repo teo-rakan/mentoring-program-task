5e0d2190894b33d9360b6ccd3d79e4e640bd2517
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-24 17:12:38 +0300

    add logger

--- src/main/java/com/epam/core/guice/modules/AndroidModule.java
+++ src/main/java/com/epam/core/guice/modules/AndroidModule.java
@@ -3,17 +3,23 @@ package com.epam.core.guice.modules;
 import com.epam.core.webdriver.AndroidDriverManager;
 import com.epam.core.webdriver.DriverManager;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.support.pagefactory.FieldDecorator;
 
 public class AndroidModule extends GuiceModule {
 
+    private static final Logger LOGGER = LogManager.getLogger(AndroidModule.class);
+
     @Override
     public DriverManager provideDriverManager() {
+        LOGGER.debug("AndroidDriverManager was provided");
         return new AndroidDriverManager();
     }
 
     @Override
     public FieldDecorator provideFieldDecorator(DriverManager driverManager) {
+        LOGGER.debug("AppiumFieldDecorator was provided");
         return new AppiumFieldDecorator(driverManager.getDriver());
     }
 }
