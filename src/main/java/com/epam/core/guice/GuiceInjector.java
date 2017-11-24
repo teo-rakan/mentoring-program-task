f2c824e83cd16f2deb13c7bfbb791f7783b80ab7
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-24 17:13:10 +0300

    add module search via reflection

--- src/main/java/com/epam/core/guice/GuiceInjector.java
+++ src/main/java/com/epam/core/guice/GuiceInjector.java
@@ -1,24 +1,62 @@
 package com.epam.core.guice;
 
-
-import com.epam.core.guice.modules.AndroidModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.lang.reflect.Constructor;
 
 
 public class GuiceInjector {
 
+    private static final Logger LOGGER = LogManager.getLogger(GuiceInjector.class);
+    private static final String PACKAGE = "com.epam.core.guice.modules.";
     private static Injector injector;
 
-    //todo synch ?
-    public static Injector getInjector() {
+    private static Class<? extends Module> findModule(String modulePartName) {
+        String capitalizedModulePartName = modulePartName.substring(0, 1).toUpperCase()
+                + modulePartName.substring(1).toLowerCase();
+        String className = PACKAGE + capitalizedModulePartName + "Module";
+
+        Class<? extends Module> module = null;
+        try {
+            module = (Class<? extends Module>) Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("Cannot find module: " + className);
+        }
+
+        return module;
+    }
+
+    private static Module createInstance(Class<? extends Module> moduleClass) {
+        Module module = null;
+
+        try {
+            Constructor constructor = moduleClass.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            module = (Module) constructor.newInstance();
+            LOGGER.debug("Instance of " + moduleClass + " was created");
+        } catch (ReflectiveOperationException e) {
+            LOGGER.error("Cannot create instance of " + moduleClass + ": "
+                    + e.getMessage());
+        }
+        return module;
+    }
+
+    public static Injector get() {
         if (injector == null) {
-            //String browserName = System.getProperty("browser.name");
+            String targetTestEnvironment = System.getProperty("browser.name");
+            Class<? extends Module> moduleClass = findModule(targetTestEnvironment);
+            Module module = createInstance(moduleClass);
 
-            Module module = new AndroidModule(); // todo Module resolver
-
-            injector = Guice.createInjector(module);
+            try {
+                injector = Guice.createInjector(module);
+            } catch (Exception ex) {
+                LOGGER.error("Cannot create injector: " + ex.getMessage());
+            }
         }
         return injector;
     }
