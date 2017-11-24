ec37cee46d0c6e84f6dcf166552f2f9233ca201d
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-24 13:49:26 +0300

    remove unused imports

--- src/main/java/com/epam/core/guice/modules/AndroidModule.java
+++ src/main/java/com/epam/core/guice/modules/AndroidModule.java
@@ -3,9 +3,6 @@ package com.epam.core.guice.modules;
 import com.epam.core.webdriver.AndroidDriverManager;
 import com.epam.core.webdriver.DriverManager;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
-import org.openqa.selenium.support.pagefactory.DefaultFieldDecorator;
 import org.openqa.selenium.support.pagefactory.FieldDecorator;
 
 public class AndroidModule extends GuiceModule {
