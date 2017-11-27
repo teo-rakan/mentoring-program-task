d5139c9af3d9b02388b79bdc79b86ce4bab87426
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-27 15:07:42 +0300

    add additional verification to the test

--- src/main/java/com/epam/tests/mobile/HomePageGlobalVerificationTest.java
+++ src/main/java/com/epam/tests/mobile/HomePageGlobalVerificationTest.java
@@ -1,11 +1,11 @@
 package com.epam.tests.mobile;
 
 import com.epam.core.Configuration;
-import com.epam.pages.HeaderLayout;
 import com.epam.pages.bean.MenuItem;
 import com.epam.pages.mobile.MobileHeaderLayout;
 import com.epam.pages.mobile.MobileHomePage;
 import com.epam.tests.BaseTest;
+import com.epam.utils.HttpUtil;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -18,21 +18,38 @@ public class HomePageGlobalVerificationTest extends BaseTest {
     @DataProvider(name = "mobile headers")
     public static Object[][] headers() {
         String baseURL = Configuration.getBaseUrl();
-        List<MenuItem> headers = new ArrayList<>();
-        headers.add(new MenuItem("HOME", baseURL + "/"));
-        headers.add(new MenuItem("SERIES", baseURL + "/series"));
-        headers.add(new MenuItem("MOVIES", baseURL + "/movies"));
-        headers.add(new MenuItem("SPORTS", baseURL + "/sports"));
-        headers.add(new MenuItem("DOCUMENTARIES", baseURL + "/documentaries"));
-        headers.add(new MenuItem("COMEDY", baseURL + "/comedy"));
-        return new Object[][]{{headers}};
+
+        List<MenuItem> primary = new ArrayList<>();
+        primary.add(new MenuItem("HOME", baseURL + "/"));
+        primary.add(new MenuItem("SERIES", baseURL + "/series"));
+        primary.add(new MenuItem("MOVIES", baseURL + "/movies"));
+        primary.add(new MenuItem("SPORTS", baseURL + "/sports"));
+        primary.add(new MenuItem("DOCUMENTARIES", baseURL + "/documentaries"));
+        primary.add(new MenuItem("COMEDY", baseURL + "/comedy"));
+
+        List<MenuItem> red = new ArrayList<>();
+        red.add(new MenuItem("TV SCHEDULE", baseURL + "/schedule"));
+        red.add(new MenuItem("ON DEMAND AND STREAMING SCHEDULE", baseURL + "/on-demand/series"));
+
+        MenuItem getShowtime = new MenuItem("GET SHOWTIME", baseURL + "/order");
+
+        return new Object[][]{{primary, red, getShowtime}};
     }
 
-    @Test(dataProvider = "mobile headers")
-    public void homePageGlobalVerificationTest(List<MenuItem> expectedHeaders) {
-        HeaderLayout homePage = new MobileHomePage().openMenu();
-        List<MenuItem> actualHeaders = homePage.getPrimaryMenuItems();
 
+    @Test(dataProvider = "mobile headers")
+    public void homePageGlobalVerificationTest(List<MenuItem> primary,
+                                               List<MenuItem> red,
+                                               MenuItem getShowtime) {
+        MobileHeaderLayout headerLayout = new MobileHomePage().openMenu();
+        verifyMenuItems(headerLayout.getPrimaryMenuItems(), primary);
+        verifyMenuItems(headerLayout.getRedMenuItems(), red);
+        verifyMenuItem(headerLayout.getGetShowtime(), getShowtime);
+        headerLayout.closeMenu();
+
+    }
+
+    private void verifyMenuItems(List<MenuItem> actualHeaders, List<MenuItem> expectedHeaders) {
         Assert.assertEquals(actualHeaders.size(), expectedHeaders.size(),
                 "Expected header count: " + expectedHeaders.size()
                         + " Found: " + actualHeaders.size());
@@ -40,8 +57,17 @@ public class HomePageGlobalVerificationTest extends BaseTest {
         for (int i = 0; i < actualHeaders.size(); i++) {
             MenuItem actualHeader = actualHeaders.get(i);
             MenuItem expectedHeader = expectedHeaders.get(i);
-            Assert.assertTrue(actualHeader.equals(expectedHeader),
-                    "Actual: " + actualHeader + "\nExpected: " + expectedHeader);
+            verifyMenuItem(actualHeader, expectedHeader);
         }
+    }
+
+    private void verifyMenuItem(MenuItem actual, MenuItem expected) {
+        String link = actual.getLink();
+        int responseCode = HttpUtil.getStatusCode(link);
+
+        Assert.assertTrue(actual.equals(expected),
+                "\nActual: " + actual + "\nExpected: " + expected);
+        Assert.assertEquals(responseCode, 200,
+                actual.getTitle() + " link is unavailable: " + link);
     }
 }
