8fa23fa2003310be2717d4bc1931745664c54a44
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-27 13:47:40 +0300

    add dataProviders with header expected information

--- src/main/java/com/epam/tests/desktop/HomePageHeaderVerificationTest.java
+++ src/main/java/com/epam/tests/desktop/HomePageHeaderVerificationTest.java
@@ -1,21 +1,47 @@
 package com.epam.tests.desktop;
 
+import com.epam.core.Configuration;
 import com.epam.pages.HeaderLayout;
 import com.epam.pages.bean.MenuItem;
 import com.epam.pages.desktop.DesktopHeaderLayout;
 import com.epam.tests.BaseTest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HomePageHeaderVerificationTest extends BaseTest {
 
-    @Test
-    public void homePageHeaderVerificationTest() {
-        HeaderLayout homePage = new DesktopHeaderLayout();
-        List<MenuItem> primaryMenuItems = homePage.getPrimaryMenuItems();
+    private static final Logger LOGGER = LogManager.getLogger(HomePageHeaderVerificationTest.class);
 
-        Assert.assertEquals(primaryMenuItems.size(), 5);
+    @DataProvider(name = "desktop headers")
+    public static Object[][] headers() {
+        String baseURL = Configuration.getBaseUrl();
+        List<MenuItem> headers = new ArrayList<>();
+        headers.add(new MenuItem("SERIES", baseURL + "/series"));
+        headers.add(new MenuItem("MOVIES", baseURL + "/movies"));
+        headers.add(new MenuItem("SPORTS", baseURL + "/sports"));
+        headers.add(new MenuItem("DOCUMENTARIES", baseURL + "/documentaries"));
+        headers.add(new MenuItem("COMEDY", baseURL + "/comedy"));
+        return new Object[][]{{headers}};
+    }
+
+    @Test(dataProvider = "desktop headers")
+    public void homePageHeaderVerificationTest(List<MenuItem> expectedHeaders) {
+        HeaderLayout homePage = new DesktopHeaderLayout();
+        List<MenuItem> actualHeaders = homePage.getPrimaryMenuItems();
+
+        Assert.assertEquals(actualHeaders.size(), expectedHeaders.size());
+
+        for (int i = 0; i < actualHeaders.size(); i++) {
+            MenuItem actualHeader = actualHeaders.get(i);
+            MenuItem expectedHeader = expectedHeaders.get(i);
+            LOGGER.debug("Actual: " + actualHeader + "\nExpected: " + expectedHeader);
+            Assert.assertTrue(actualHeader.equals(expectedHeader));
+        }
     }
 }
--- src/main/java/com/epam/tests/mobile/HomePageGlobalVerificationTest.java
+++ src/main/java/com/epam/tests/mobile/HomePageGlobalVerificationTest.java
@@ -1,21 +1,47 @@
 package com.epam.tests.mobile;
 
+import com.epam.core.Configuration;
 import com.epam.pages.HeaderLayout;
 import com.epam.pages.bean.MenuItem;
+import com.epam.pages.mobile.MobileHeaderLayout;
 import com.epam.pages.mobile.MobileHomePage;
 import com.epam.tests.BaseTest;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HomePageGlobalVerificationTest extends BaseTest {
 
-    @Test
-    public void homePageGlobalVerificationTest() {
-        HeaderLayout homePage = new MobileHomePage().openMenu();
-        List<MenuItem> menuItems = homePage.getPrimaryMenuItems();
+    @DataProvider(name = "mobile headers")
+    public static Object[][] headers() {
+        String baseURL = Configuration.getBaseUrl();
+        List<MenuItem> headers = new ArrayList<>();
+        headers.add(new MenuItem("HOME", baseURL + "/"));
+        headers.add(new MenuItem("SERIES", baseURL + "/series"));
+        headers.add(new MenuItem("MOVIES", baseURL + "/movies"));
+        headers.add(new MenuItem("SPORTS", baseURL + "/sports"));
+        headers.add(new MenuItem("DOCUMENTARIES", baseURL + "/documentaries"));
+        headers.add(new MenuItem("COMEDY", baseURL + "/comedy"));
+        return new Object[][]{{headers}};
+    }
 
-        Assert.assertEquals(menuItems.size(), 6);
+    @Test(dataProvider = "mobile headers")
+    public void homePageGlobalVerificationTest(List<MenuItem> expectedHeaders) {
+        HeaderLayout homePage = new MobileHomePage().openMenu();
+        List<MenuItem> actualHeaders = homePage.getPrimaryMenuItems();
+
+        Assert.assertEquals(actualHeaders.size(), expectedHeaders.size(),
+                "Expected header count: " + expectedHeaders.size()
+                        + " Found: " + actualHeaders.size());
+
+        for (int i = 0; i < actualHeaders.size(); i++) {
+            MenuItem actualHeader = actualHeaders.get(i);
+            MenuItem expectedHeader = expectedHeaders.get(i);
+            Assert.assertTrue(actualHeader.equals(expectedHeader),
+                    "Actual: " + actualHeader + "\nExpected: " + expectedHeader);
+        }
     }
 }
