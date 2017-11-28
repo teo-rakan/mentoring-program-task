af388a4b45e9e0a70149dbdf2abd96ec61c34d54
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-28 12:38:23 +0300

    add light red color for mobile

--- src/main/java/com/epam/pages/bean/RGBAColor.java
+++ src/main/java/com/epam/pages/bean/RGBAColor.java
@@ -3,7 +3,8 @@ package com.epam.pages.bean;
 public enum RGBAColor {
     BLACK("rgba(0, 0, 0, 0)"),
     WHITE("rgba(255, 255, 255, 1)"),
-    RED("rgba(177, 0, 0, 1)");
+    RED("rgba(177, 0, 0, 1)"),
+    LIGHT_RED("rgba(201, 20, 31, 1)");
 
     private String rgbaValue;
 
