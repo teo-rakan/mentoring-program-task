67f2614eafcb3f7c19f0ed3b4b06eb173568dc82
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-28 11:15:55 +0300

    update RED color rgba value

--- src/main/java/com/epam/pages/bean/RGBAColor.java
+++ src/main/java/com/epam/pages/bean/RGBAColor.java
@@ -3,7 +3,7 @@ package com.epam.pages.bean;
 public enum RGBAColor {
     BLACK("rgba(0, 0, 0, 0)"),
     WHITE("rgba(255, 255, 255, 1)"),
-    RED("rgba(201, 20, 31, 1)");
+    RED("rgba(177, 0, 0, 1)");
 
     private String rgbaValue;
 
