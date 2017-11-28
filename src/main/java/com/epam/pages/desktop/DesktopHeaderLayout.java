e9adffcabd184f6ba97f94abd5b7d369c1532443
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-28 17:44:09 +0300

    add debug logs

--- src/main/java/com/epam/pages/desktop/DesktopHeaderLayout.java
+++ src/main/java/com/epam/pages/desktop/DesktopHeaderLayout.java
@@ -2,12 +2,16 @@ package com.epam.pages.desktop;
 
 import com.epam.pages.HeaderLayout;
 import com.epam.pages.entity.MenuItem;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
 
 public class DesktopHeaderLayout extends HeaderLayout {
+
+    private static final Logger LOGGER = LogManager.getLogger(DesktopHeaderLayout.class);
 
     @FindBy(xpath = "//nav[@data-context='global navigation']")
     private WebElement header;
@@ -27,10 +31,12 @@ public class DesktopHeaderLayout extends HeaderLayout {
 
     @Override
     public List<MenuItem> getPrimaryMenuItems() {
+        LOGGER.debug("Get primary menu items as list [title, link]");
         return convertIfVisible(headerPrimaryMenuItems);
     }
 
     public List<MenuItem> getRightMenuItems() {
+        LOGGER.debug("Get right menu items as list [title, link]");
         return convertIfVisible(headerRightMenuItems);
     }
 }
--- src/main/java/com/epam/pages/desktop/FooterLayout.java
+++ src/main/java/com/epam/pages/desktop/FooterLayout.java
@@ -1,10 +1,14 @@
 package com.epam.pages.desktop;
 
 import com.epam.pages.BasePage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class FooterLayout extends BasePage {
+
+    private static final Logger LOGGER = LogManager.getLogger(FooterLayout.class);
 
     @FindBy(className = "footer__logo")
     private WebElement logo;
@@ -22,26 +26,31 @@ public class FooterLayout extends BasePage {
     private WebElement tryItNowForFreeLink;
 
     public boolean isLogoVisible() {
+        LOGGER.debug("Check Showtime logo visibility");
         driverManager.waitUntilVisible(logo);
         return logo.isDisplayed();
     }
 
     public boolean isHaveShowtimeHeaderVisible() {
+        LOGGER.debug("Check 'Have Showtime?' header visibility");
         driverManager.waitUntilVisible(haveShowtimeHeader);
         return haveShowtimeHeader.isDisplayed();
     }
 
     public boolean isWantShowtimeSubscribeHeaderVisible() {
+        LOGGER.debug("Check 'Want Showtime? Subscribe.' header visibility");
         driverManager.waitUntilVisible(wantShowtimeSubscribeHeader);
         return wantShowtimeSubscribeHeader.isDisplayed();
     }
 
     public String getStreamShowtimeLink() {
+        LOGGER.debug("Get 'Stream Showtime' link");
         driverManager.waitUntilVisible(streamShowtimeLink);
         return streamShowtimeLink.getAttribute("href");
     }
 
     public String getTryItNowForFreeLink() {
+        LOGGER.debug("Get 'Try it for now for free' link");
         driverManager.waitUntilVisible(tryItNowForFreeLink);
         return tryItNowForFreeLink.getAttribute("href");
     }
