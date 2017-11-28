6629af521dd7385955e3e71b88cb58e256e5972d
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-28 15:26:40 +0300

    create Order page for mobile

---
+++ src/main/java/com/epam/pages/mobile/OrderPage.java
@@ -0,0 +1,50 @@
+package com.epam.pages.mobile;
+
+import com.epam.pages.BasePage;
+import com.epam.pages.bean.Link;
+import com.epam.pages.bean.MenuItem;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderPage extends BasePage {
+
+    private static final Logger LOGGER = LogManager.getLogger(OrderPage.class);
+
+    @FindBy(className = "order-tray__body-headline")
+    private WebElement title;
+
+    @FindBy(className = "order-card__logo")
+    private List<WebElement> providerList;
+
+    public String getTitle() {
+        driverManager.waitUntilVisible(title);
+        return title.getText();
+    }
+
+    public List<Link> getAllProviderLinks() {
+        LOGGER.debug("Get providers as list [alt name, src link]");
+        driverManager.waitUntilVisible(title);
+        return convertToLinks(providerList);
+    }
+
+    private List<Link> convertToLinks(List<WebElement> images) {
+        List<Link> entityList = new ArrayList<>();
+
+        for (WebElement image : images) {
+            LOGGER.debug(image);
+            entityList.add(convertToLink(image));
+        }
+        return entityList;
+    }
+
+    private Link convertToLink(WebElement image) {
+        return new Link(image.getAttribute("alt"),
+                image.getAttribute("src"));
+    }
+}
