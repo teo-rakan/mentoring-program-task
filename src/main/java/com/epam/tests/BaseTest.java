eadec979e50848da5d1a7219e4f1639184f113a4
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-29 18:16:37 +0300

    remove @BeforeSuite and @AfterSuite

--- src/main/java/com/epam/tests/BaseTest.java
+++ src/main/java/com/epam/tests/BaseTest.java
@@ -1,13 +1,10 @@
 package com.epam.tests;
 
-import com.epam.core.Configuration;
 import com.epam.core.guice.GuiceInjector;
 import com.epam.core.webdriver.DriverManager;
 import com.epam.utils.HttpUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
 
 import javax.inject.Inject;
 
@@ -23,20 +20,6 @@ public class BaseTest {
 
     public BaseTest() {
         GuiceInjector.get().injectMembers(this);
-    }
-
-    @BeforeSuite
-    public void openBrowser() {
-        try {
-            driverManager.open(Configuration.getBaseUrl());
-        } catch (Exception ex) {
-            LOGGER.error(ex.getMessage());
-        }
-    }
-
-    @AfterSuite
-    public void closeBrowser() {
-        driverManager.quit();
     }
 
     protected void verifyLink(String title, String link) {
