9879e6333e67a9fdc31d1fc4f6008809a4286f22
Alena Zubrevich <alena_zubrevich@epam.com>
2017-11-29 18:17:30 +0300

    add synchronization for driver creation/quiting

--- src/main/java/com/epam/core/webdriver/DriverManager.java
+++ src/main/java/com/epam/core/webdriver/DriverManager.java
@@ -1,6 +1,6 @@
 package com.epam.core.webdriver;
 
-import com.epam.core.guice.GuiceInjector;
+import com.epam.utils.ScreenshotUtil;
 import com.epam.utils.WaitUtil;
 import com.google.inject.Inject;
 import org.apache.logging.log4j.LogManager;
@@ -14,14 +14,22 @@ public class DriverManager {
 
     private static final Logger LOGGER = LogManager.getLogger(DriverManager.class);
 
+    private DriverFactory driverFactory;
     private WebDriver driver;
 
     public DriverManager(DriverFactory driverFactory) {
-        LOGGER.debug("New DriverManager creating...");
-        driver = driverFactory.createDriver();
+        this.driverFactory = driverFactory;
     }
 
     public WebDriver getDriver() {
+        if (null == driver) {
+            synchronized (this) {
+                if (null == driver) {
+                    driver = driverFactory.createDriver();
+                    LOGGER.debug("Driver was successfully created");
+                }
+            }
+        }
         return driver;
     }
 
@@ -32,9 +40,13 @@ public class DriverManager {
 
     public void quit() {
         if (null != driver) {
-            driver.quit();
-            driver = null;
-            LOGGER.debug("Driver quited");
+            synchronized (this) {
+                if (null != driver) {
+                    driver.quit();
+                    driver = null;
+                    LOGGER.debug("Driver successfully quited");
+                }
+            }
         }
     }
 
@@ -43,13 +55,16 @@ public class DriverManager {
     }
 
     public void waitUntilVisible(WebElement element) {
-        WaitUtil.untilVisible(element, driver);
+        WaitUtil.untilVisible(element, getDriver());
     }
 
     public void scrollToThePageBottom() {
-        JavascriptExecutor js = ((JavascriptExecutor) driver);
+        JavascriptExecutor js = ((JavascriptExecutor) getDriver());
         js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
     }
 
+    public void takeScreenshot() {
+        ScreenshotUtil.take(getDriver());
+    }
 }
 
